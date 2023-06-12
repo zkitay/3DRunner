ddcc7baed9f4c670046033ea6ed242c45adb9585
zkitay <134313077+zkitay@users.noreply.github.com>
2023-06-12 21:33:50 +0300

    added camera follow

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,10 @@ public class PlayerController : MonoBehaviour
 {
 
     public float runningSpeed;
-
+    float touchXDelta = 0;
+    float newX = 0;
+    public float xSpeed;
+    public float limitx;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+        SwipeCheck();
+    }
+
+    private void SwipeCheck()
+    {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+        else 
+        {
+            touchXDelta = 0;
+        }
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX, -limitx, limitx);
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
         transform.position = newPosition;
+
     }
 }
